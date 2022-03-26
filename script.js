5652764f4c2442601a82eb073e2fde654da795a4
AmandaLSmithlin <AmandaLSmithlin@gmail.com>
2022-03-26 10:06:31 -0400

    added js functions to generate random password

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ let buttonHandler = () => {
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+
+function addNewPassword() {
+    const randomArray = [
+         //prettier-ignore
+         ['~', '`', '!', '@', '$', '%','^','&', '*','(',')','-','_','+','=','#'],
+         //prettier-ignore
+         [0,1,2,3,4,5,6,7,8,9],
+         //prettier-ignore
+         ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+         //prettier-ignore
+         ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    ];
+       
+
+     const pwLength = Math.floor(Math.random() * 9) + 10;
+    let randomPassword = [];
+
+    while (randomPassword.length <= pwLength) {
+        let randomIndex = randomArray[Math.floor(Math.random() * randomArray.length)];
+        let randomSelector = Math.floor(Math.random()* randomIndex.length);
+        randomPassword.push(randomIndex[randomSelector]);
+
+    }
+   //console.log(randomArray.length);
+   //console.log(randonIndex);
+  // console.log(randomSelector);
+   console.log(randomPassword.join(""));
+   return randomPassword.join("");
+}
